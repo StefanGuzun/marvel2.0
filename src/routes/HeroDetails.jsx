b1790d102f3011b3ad932b7905317f173a75b2e4
stefan <stefanguzun2005@gmail.com>
2022-07-06 21:56:21 +0300

    added the char detailed info page

--- src/routes/HeroDetails.jsx
+++ src/routes/HeroDetails.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useParams } from "react-router-dom"
+import { fetchHero } from '../utils/utils';
 
 export default function HeroDetails() {
+  let {id} = useParams();
+
+  const [hero, setHero] = useState()
+
+  useEffect(() => {
+    fetchHero(id)
+    .then(data => setHero(data[0]))
+    .catch(err => console.log(err))
+  })
+
+  if (!hero) return
   return (
-    <div>HeroDetails</div>
+    <div  className="container">
+      <div style={{backgroundColor: "antiquewhite", display: "flex", }} className="hero__detailsCont">
+        <img style={{width: 500, height: 500}} src={`${hero.thumbnail.path}.${hero.thumbnail.extension}`}/>
+        <div style={{display: "flex", flexDirection: "column"}} className="heroDetails">
+          <h4>Name:</h4>
+          <p>{hero.name}</p>
+          {hero.description ? (<>
+          <h4>description:</h4>
+          <p>{hero.description}</p>
+          </>): null}
+          <div className="heroSeries">
+            <h4>series:</h4>
+            <ul>
+              {
+                hero.series.items.map(item => <li key={Math.random() * 1000}>{item.name}</li>)
+              }
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
