6ee3ce020f14f7b2d68972f89216bc4cbfd0a04b
Adelina <tapesadelina@gmail.com>
2022-07-06 17:35:16 +0300

    header and background styling

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,14 @@
 import React, {useState, useEffect} from "react";
-import logo from "../../img/marvel-logo.png"
-;
-
+import logo from "../../img/marvel-logo.png";
+import "./Header.css"
 
 export const Header = () => {
 
     return(
-        <header className="center">
-            <img src={logo}/>
-        </header>
+        <div>
+            <div className="headerStyle">
+                <img src={logo} alt="marvel-logo" className="logoImage"/>
+            </div>
+        </div>
     )
 }
