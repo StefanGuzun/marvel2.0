0b40c71f35f835a0d75d73e199819ee2708d9a85
Ciprian <moraruciprian2005@gmail.com>
2022-07-06 14:35:23 +0300

    i added marvel api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import {Header} from "./Components/Header/Header";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import MD5 from "crypto-js/md5"
+
+let MarvelBasicUrl = "http://gateway.marvel.com"
+let baseUrl = `${MarvelBasicUrl}/v1/public/characters`
+let privateKey = "e628d2c69acb9a2c79419d570c0b7e7c5a51134a"
+let publicKey = "ee11e0020252facff7de0487eee802fd"
+let ts = Date.now().toString()
+const getHash = (ts, privateKey, publicKey) => {
+    return MD5(ts + privateKey + publicKey).toString()
+}
+let hash = getHash(ts, privateKey, publicKey)
+let finalUrl = `${baseUrl}?ts=${ts}&apikey=${publicKey}&hash=${hash}`
+console.log(hash)
+
 
 function App() {
+    useEffect(()=>{
+        const fetch = async () => {
+            const result = await axios(finalUrl)
+            console.log(result.data)
+        }
+        fetch()
+    }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header/>
     </div>
   );
 }
