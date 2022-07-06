9420b21cd59544c18bd9bcd42428e1027bf805a0
DoomPin <pynzaru.dima@mail.ru>
2022-07-06 21:01:29 +0300

    SearchBar 50% raady

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import './App.css';
 import {Header} from "./Components/Header/Header";
+import SearchBar from './Components/SearchBar/SearchBar';
 import React, {useEffect, useState} from "react";
 import axios from "axios";
 import MD5 from "crypto-js/md5"
 
 let MarvelBasicUrl = "http://gateway.marvel.com"
 let baseUrl = `${MarvelBasicUrl}/v1/public/characters`
-let privateKey = "e628d2c69acb9a2c79419d570c0b7e7c5a51134a"
-let publicKey = "ee11e0020252facff7de0487eee802fd"
+let privateKey = "333b80168c7b4eade803b339a942fd6d9bc82c3c"
+let publicKey = "5acfe49a16e8e56d0d8292e1f7f13dae"
 let ts = Date.now().toString()
 const getHash = (ts, privateKey, publicKey) => {
     return MD5(ts + privateKey + publicKey).toString()
 }
 let hash = getHash(ts, privateKey, publicKey)
 let finalUrl = `${baseUrl}?ts=${ts}&apikey=${publicKey}&hash=${hash}`
-console.log(hash)
 
 
 function App() {
     useEffect(()=>{
         const fetch = async () => {
             const result = await axios(finalUrl)
-            console.log(result.data)
+            console.log(result.data.data.results)
         }
         fetch()
     }, [])
@@ -29,6 +29,7 @@ function App() {
   return (
     <div className="container">
       <Header/>
+      <SearchBar/>
     </div>
   );
 }
