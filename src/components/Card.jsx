d5eb3b3cf15ffaf81ae6a6f8f19ca7ea886086c3
stefan <stefanguzun2005@gmail.com>
2022-07-07 15:43:26 +0300

    i modified the design

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,7 @@ export default function Card({id, thumbnail, name}) {
     <Link className="charInfoCard" style={{textDecoration:"none"}} to={`/${id}`}>
     <div style={{textAlign: "center"}}>
         <img style={{margin: 10}} src={thumbnail} alt="thumbnail"/>
-        <p style={{color: "black", backgroundColor: "white", fontSize: 16}}>{name}</p>
+        <p style={{color: "black", backgroundColor: "white",maxWidth: 300, borderRadius: 50}}>{name}</p>
     </div>
     </Link>
   )
