798f251186283a02c0ccb016277e7d809905c11b
sjnt <bragav731@gmail.com>
2022-07-07 12:17:12 +0300

    fixed file Homes

--- src/routes/Homes.jsx
+++ src/routes/Homes.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Grid from '../components/Grid'
 import Container from '../components/Container'
 import Card from '../components/Card'
+import SearchBar from "../components/SearchBar"
+
+const IMAGE_SIZE = "standart_fantastic"
 
 export default function Homes() {
     const [heroes, setHeroes] = useState([])
 
-    let cards
+    let cards;
 
     if (heroes){
         cards = heroes.map((hero) => {
             <Card
             name={hero.name} 
             id={hero.id}
-            thumbnail={`${hero.thumbnail.path}.${hero.thumbnail.extension}`}
+            key={hero.id}
+            thumbnail={`${hero.thumbnail.path}/${IMAGE_SIZE}.${hero.thumbnail.extension}`}
             />
         })
     }
-    
+
   return (
     <Container>
         <h1>marvel heroes</h1>
